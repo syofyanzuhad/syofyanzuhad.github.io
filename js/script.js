3f3d1405a2c02b3751e0658b7b3c20a8fa761ec0
syofyanzuhad <sofyanzuhad2@gmail.com>
2019-11-04 13:27:32 +0700

    photography, curriculum vitae

--- js/script.js
+++ js/script.js
@@ -88,20 +88,25 @@ function closeNav() {
 
         function openSlideUp2() {
             document.getElementById("photography").style.height = "100vh";
+            document.getElementById('closebtn2').style.position = "fixed";
         }
 
         function openSlideUp() {
             document.getElementById("portfolio").style.height = "100vh";
+            document.getElementById('closebtn1').style.position = "fixed";
         }
         
 
         /* Set the width of the side navigation to 0 */
         function closeSlideUp() {
             document.getElementById("portfolio").style.height = "0";
+            document.getElementById('closebtn2').style.position = "absolute";
+
         }
 
         function closeSlideUp2() {
             document.getElementById("photography").style.height = "0";
+            document.getElementById('closebtn1').style.position = "absolute";
         }
 
         function closeRightNav() {
@@ -117,7 +122,7 @@ function closeNav() {
             document.getElementById("photography").style.height = "0";
             document.getElementById("about").style.width = "0";
             document.getElementById("contacts").style.width = "0";
-
+            document.getElementById('closebtn').style.position = "absolute";
         }
 
     // }
