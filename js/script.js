ff5d91eaf7181ea2679418709e41b0b4448c64d6
syofyanzuhad <sofyanzuhad2@gmail.com>
2019-11-06 11:08:09 +0700

    add project

--- js/script.js
+++ js/script.js
@@ -132,3 +132,36 @@ function closeNav() {
 // myFunction(x) // Call listener function at run time
 // x.addListener(myFunction) // Attach listener function on state changes
 
+//////////// click event to hide overlay ////////////
+var $overlay = document.getElementsByClassName('overlay');
+
+$overlay.click(function(){ //annonymous function to hide overlay
+    console.log('overlay clicked');
+    $overlay.hide();
+});
+
+//////////// add overlay to html ////////////
+// var $overlay = $('<div id="overlay"></div>'); //string to create div (overlay) as jQuery variable
+// var $image = $('<img>'); //select image tag as jQuery variable
+// var $caption = $('<p></p>');
+
+// $overlay.append($image); //add image to overlay
+// $overlay.append($caption); //add caption to overlay
+// $('body').append($overlay); //add overlay to body
+
+
+// //////////// capture click event on image link ////////////
+// $('#card a').click(function(event){ //annonymous function with event parameter
+//     event.preventDefault(); //stops default event from happening (image loading)
+//     var imageLocation = $(this).attr('href'); //gets href attribute from clicked 'a' tag in imageGallery
+//     $image.attr('src', imageLocation); //set src attribute to imageLocation variable
+//     $overlay.show(); //shows the overlay
+//     var captionText = $(this).children('img').attr('alt'); //get the childs alt attribute
+//     $caption.text(captionText);
+// });
+
+
+// //////////// click event to hide overlay ////////////
+// $overlay.click(function(){ //annonymous function to hide overlay
+//     $overlay.hide();
+// });
