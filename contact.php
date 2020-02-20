215136f90ea7b7c5772551e2392b0256fcf534ff
syofyanzuhad <sofyanzuhad2@gmail.com>
2020-02-20 22:08:07 +0700

    meta tag google

--- contact.php
+++ contact.php
@@ -1,11 +1,11 @@
 <?php
 
-if (isset($_POST['kirim'])) {
+if (isset($_POST['p_email'])) {
     $to = "sofyanzuhad2@gmail.com";
     $subject = "YOU'VE NEW REPORT !";
     $nama = $_POST['nama'];
     $email = $_POST['email'];
-    $pesan = $nama." Mengirim anda pesan. "."\n"."Pesan : "."\n". $_POST['pesan'];
+    $pesan = $nama." Mengirim anda pesan. "."\n"."Pesan : "."\n". $_POST['pesan_email'];
     
     if (mail($to, $subject, $pesan)) {
         echo "" ?> <div class="alert alert-success fixed-top text-center" role="alert">EMAIL SENDED ! Thank You !</div> <?php
@@ -13,3 +13,5 @@ if (isset($_POST['kirim'])) {
       echo "" ?> <div class="alert alert-danger  text-center" role="alert">EMAIL FAILED ! (correct your email !)</div> <?php
     }
 }
+
+?>
