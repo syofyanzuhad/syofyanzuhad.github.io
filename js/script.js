ae5b5ec127ba154ac75bd12678d6f4d52b0d4ae6
syofyanzuhad <sofyanzuhad2@gmail.com>
2020-08-31 11:40:43 +0700

    fixed =>dzikir v 1.0

--- js/script.js
+++ js/script.js
@@ -57,6 +57,30 @@ window.onload = function() {
     document.body.appendChild(css);
 };
 
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
+    }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+
 //      Full Width SideNav
 
 /* Open the sidenav 
